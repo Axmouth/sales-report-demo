86de1a4045d4b64f351736ea65843b012d815735
Axmouth <aggronfg@yahoo.com>
2020-02-29 16:44:19 +0200

    Added docker support, rerranged folder structure

---
+++ server/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var salesRouter = require('./routes/sales');
+
+mongoose.connect(process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+  process.env.MONGODOCKER_URI ||
+  'mongodb://mongo/sales-report-demo?ssl=true', {
+    promiseLibrary: require('bluebird'),
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    ssl: false,
+    server: {ssl: false},
+}).then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', salesRouter);
+
+module.exports = app;
